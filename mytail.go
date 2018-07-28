8297b7468b032e1d6052d2ee7760597d7d6444e0
shutaka9576 <shuntaka9576@gmail.com>
2018-07-28 17:53:00 +0900

    複数ファイル指定機能と引数周りを改善

--- mytail.go
+++ mytail.go
@@ -2,70 +2,72 @@ package main
 
 import (
 	//"fmt"
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
-	"os"
-	"strings"
 	"log"
-)
-
-var (
-	linesNum = flag.Int("n", 10, "output the last N lines")
+	"os"
 )
 
 func main() {
+	var (
+		linesNum            = flag.Int("n", 10, "output the last N lines")
+		ignoreBlankLineFlag = flag.Bool("v", false, "output the last N lines")
+	)
 	flag.Parse()
+	fileName := flag.Args()
 
-	fileName := ""
-	switch len(os.Args) {
-	case 2:
-		fileName = os.Args[1]
-	case 4:
-		fileName = os.Args[3]
-	default:
-		log.Fatalf("args error please: mytail -help")
-	}
-
-	mytail(fileName, *linesNum, os.Stdout, 1024)
+	mytail(fileName, *ignoreBlankLineFlag, *linesNum, os.Stdout, 1024)
 }
 
-func mytail(fileName string, N int, output io.Writer, bufsize int64) {
-	fp, err := os.Open(fileName)
-	if err != nil {
-		log.Fatalf("file open error %v\n", err)
-	}
+func mytail(fileNames []string, ignoreBlankLineFlag bool, N int, output io.Writer, bufsize int64) {
+	for _, fileName := range fileNames {
+		if len(fileNames) > 1 {
+			fmt.Printf("==> %v <==\n", fileName)
+		}
+		fp, err := os.Open(fileName)
+		if err != nil {
+			log.Fatalf("file(%v) open error %v\n", fileName, err)
+		}
 
-	info, err := fp.Stat()
-	if err != nil {
-		log.Fatalf("get fileinfo error %v\n", err)
-	}
+		info, err := fp.Stat()
+		if err != nil {
+			log.Fatalf("get fileinfo error %v\n", err)
+		}
 
-	size := info.Size()
+		size := info.Size()
 
-	lines := ""
+		lines := ""
 
-	for N != 0 {
-		buf := make([]byte, bufsize)
-		fp.Seek(size-bufsize, 0)
-		_, _ = fp.Read(buf)
+		for N != 0 {
+			buf := make([]byte, bufsize)
+			fp.Seek(size-bufsize, 0)
+			_, _ = fp.Read(buf)
 
-		num := strings.Count(string(buf), "\n")
-
-		if N <= num {
-			for i := len(buf) - 1; i >= 0; i-- {
-				if buf[i] == 10 {
-					N--
-					if N == 0 {
-						fmt.Fprint(output, string(buf[i+1:])+lines)
-						break
-					}
+			if ignoreBlankLineFlag {
+				fmt.Println(bytes.Count(buf, []byte("\n\n")))
+				for bytes.Count(buf, []byte("\n\n")) != 0 {
+					buf = bytes.Replace(buf, []byte("\n\n"), []byte("\n"), -1)
 				}
 			}
-		} else {
-			N -= num
-			lines = string(buf) + lines
+			num := bytes.Count(buf, []byte("\n"))
+
+			if N <= num {
+				for i := len(buf) - 1; i >= 0; i-- {
+					if buf[i] == 10 {
+						N--
+						if N == 0 {
+							fmt.Fprint(output, string(buf[i+1:])+lines)
+							break
+						}
+					}
+				}
+			} else {
+				N -= num
+				lines = string(buf) + lines
+			}
+			size -= bufsize
 		}
-		size -= bufsize
 	}
 }
--- mytail_test.go
+++ mytail_test.go
@@ -1,50 +1,70 @@
 package main
 
 import (
-	"testing"
 	"bytes"
+	"os"
+	"testing"
 )
 
+/*
+	test
+*/
+
+func TestMytailStandardoutPut3(t *testing.T) {
+	mytail("testlog.log", false, 3, os.Stdout, 10)
+}
+
+func TestMytailStandardOutput10(t *testing.T) {
+	mytail("testlog.log", false, 10, os.Stdout, 10)
+}
+
+func TestMytailBlackLine(t *testing.T) {
+	mytail("./testlog/blankline.log", true, 3, os.Stdout, 10)
+}
+
+/*
+	benchmark
+*/
 const benchN = 100
 
 func BenchmarkTailBuf1(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", benchN, &bytes.Buffer{}, 1)
+		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 1)
 	}
 }
 
 func BenchmarkTailBufbench10(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", benchN, &bytes.Buffer{}, 10)
+		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 10)
 	}
 }
 
 func BenchmarkTailBuf1024(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", benchN, &bytes.Buffer{}, 1024)
+		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 1024)
 	}
 }
 
 func BenchmarkTailBuf2048(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", benchN, &bytes.Buffer{}, 2048)
+		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 2048)
 	}
 }
 
 func BenchmarkTailBuf4000(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", benchN, &bytes.Buffer{}, 4000)
+		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 4000)
 	}
 }
 
 func BenchmarkTailBuf10240(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", benchN, &bytes.Buffer{}, 10240)
+		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 10240)
 	}
 }
