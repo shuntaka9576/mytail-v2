338942335ff2d156d7150971f235ba8f15993d0b
shutaka9576 <shuntaka9576@gmail.com>
2018-07-28 18:20:24 +0900

    リファクタ＆fp.Read時に取得できないbyteが0になっていましnullとして出力されるバグを修正

--- mytail.go
+++ mytail.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	//"fmt"
 	"bytes"
 	"flag"
 	"fmt"
@@ -16,58 +15,58 @@ func main() {
 		ignoreBlankLineFlag = flag.Bool("v", false, "output the last N lines")
 	)
 	flag.Parse()
-	fileName := flag.Args()
+	fileNames := flag.Args()
 
-	mytail(fileName, *ignoreBlankLineFlag, *linesNum, os.Stdout, 1024)
-}
-
-func mytail(fileNames []string, ignoreBlankLineFlag bool, N int, output io.Writer, bufsize int64) {
 	for _, fileName := range fileNames {
 		if len(fileNames) > 1 {
 			fmt.Printf("==> %v <==\n", fileName)
 		}
-		fp, err := os.Open(fileName)
-		if err != nil {
-			log.Fatalf("file(%v) open error %v\n", fileName, err)
-		}
+		mytail(fileName, *ignoreBlankLineFlag, *linesNum, os.Stdout, 1024)
+	}
+}
 
-		info, err := fp.Stat()
-		if err != nil {
-			log.Fatalf("get fileinfo error %v\n", err)
-		}
+func mytail(fileName string, ignoreBlankLineFlag bool, N int, output io.Writer, bufsize int64) {
+	fp, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("file(%v) open error %v\n", fileName, err)
+	}
 
-		size := info.Size()
+	info, err := fp.Stat()
+	if err != nil {
+		log.Fatalf("get fileinfo error %v\n", err)
+	}
 
-		lines := ""
+	size := info.Size()
 
-		for N != 0 {
-			buf := make([]byte, bufsize)
-			fp.Seek(size-bufsize, 0)
-			_, _ = fp.Read(buf)
+	lines := ""
 
-			if ignoreBlankLineFlag {
-				fmt.Println(bytes.Count(buf, []byte("\n\n")))
-				for bytes.Count(buf, []byte("\n\n")) != 0 {
-					buf = bytes.Replace(buf, []byte("\n\n"), []byte("\n"), -1)
-				}
-			}
-			num := bytes.Count(buf, []byte("\n"))
+	for N != 0 {
+		buf := make([]byte, bufsize)
+		fp.Seek(size-bufsize, 0)
+		count, _ := fp.Read(buf)
 
-			if N <= num {
-				for i := len(buf) - 1; i >= 0; i-- {
-					if buf[i] == 10 {
-						N--
-						if N == 0 {
-							fmt.Fprint(output, string(buf[i+1:])+lines)
-							break
-						}
+		//if ignoreBlankLineFlag {
+		//	fmt.Println(bytes.Count(buf, []byte("\n\n")))
+		//	for bytes.Count(buf, []byte("\n\n")) != 0 {
+		//		buf = bytes.Replace(buf, []byte("\n\n"), []byte("\n"), -1)
+		//	}
+		//}
+		num := bytes.Count(buf, []byte("\n"))
+
+		if N <= num {
+			for i := count - 1; i >= 0; i-- {
+				if buf[i] == 10 {
+					N--
+					if N == 0 {
+						fmt.Fprint(output, string(buf[i+1:count])+lines)
+						break
 					}
 				}
-			} else {
-				N -= num
-				lines = string(buf) + lines
 			}
-			size -= bufsize
+		} else {
+			N -= num
+			lines = string(buf) + lines
 		}
+		size -= bufsize
 	}
 }
