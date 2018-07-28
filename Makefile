d5b146d33bc6d4d3cfec9c773de18c4229487e5d
shutaka9576 <shuntaka9576@gmail.com>
2018-07-28 16:45:54 +0900

    bench追加

--- Makefile
+++ Makefile
@@ -1,1 +1,24 @@
-package main
+# Go パラメータ
+GOCMD=go
+GOBUILD=$(GOCMD) build
+GOCLEAN=$(GOCMD) clean
+GOTEST=$(GOCMD) test
+GOGET=$(GOCMD) get
+BINARY_NAME=mytail
+BINARY_UNIX=$(BINARY_NAME)_unix
+
+all: test build
+build:
+    $(GOBUILD) -o $(BINARY_NAME) -v
+test:
+    $(GOTEST) -v ./...
+clean:
+    $(GOCLEAN)
+    rm -f $(BINARY_NAME)
+    rm -f $(BINARY_UNIX)
+run:
+    $(GOBUILD) -o $(BINARY_NAME) -v ./...
+    ./$(BINARY_NAME)
+deps:
+    $(GOGET) github.com/markbates/goth
+    $(GOGET) github.com/markbates/pop
--- mytail_test.go
+++ mytail_test.go
@@ -2,39 +2,49 @@ package main
 
 import (
 	"testing"
+	"bytes"
 )
+
+const benchN = 100
 
 func BenchmarkTailBuf1(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail(1)
+		mytail("testlog.log", benchN, &bytes.Buffer{}, 1)
 	}
 }
 
-func BenchmarkTailBuf100(b *testing.B) {
+func BenchmarkTailBufbench10(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail(100)
+		mytail("testlog.log", benchN, &bytes.Buffer{}, 10)
 	}
 }
 
 func BenchmarkTailBuf1024(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail(1024)
+		mytail("testlog.log", benchN, &bytes.Buffer{}, 1024)
 	}
 }
 
 func BenchmarkTailBuf2048(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail(2048)
+		mytail("testlog.log", benchN, &bytes.Buffer{}, 2048)
+	}
+}
+
+func BenchmarkTailBuf4000(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mytail("testlog.log", benchN, &bytes.Buffer{}, 4000)
 	}
 }
 
 func BenchmarkTailBuf10240(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		mytail(10240)
+		mytail("testlog.log", benchN, &bytes.Buffer{}, 10240)
 	}
 }
