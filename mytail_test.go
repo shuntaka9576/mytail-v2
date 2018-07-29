b320bf7f3e1615de65a709399b2fc3dd6176db33
shutaka9576 <shuntaka9576@gmail.com>
2018-07-29 10:12:10 +0900

    テスト作成中

--- mytail_test.go
+++ mytail_test.go
@@ -13,19 +13,25 @@ func TestMyTail(t *testing.T) {
 		bufsize             int64
 	}
 	var tests = []struct {
-		no int
 		in   input
 		want string
 	}{
-		{1, input{"./testlog/tailtestLF.log", false, 3, 10}, "abcdefghi\nabcdefghij\n\n"},
-		{2, input{"./testlog/tailtestLF.log", false, 15, 10}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\n\nabcdefgh\n\nabcdefghi\nabcdefghij\n\n"},
+		{input{"./testlog/tailtestLF.log", false, 3, 10}, "abcdefghi\nabcdefghij\n\n"},
+		{input{"./testlog/tailtestLF.log", false, 15, 10}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\n\nabcdefgh\n\nabcdefghi\nabcdefghij\n\n"},
+		{input{"./testlog/tailtestLF.log", true, 3, 10}, "abcdefgh\nabcdefghi\nabcdefghij\n"},
+		{input{"./testlog/tailtestLF.log", true, 4, 10}, "abcdefg\nabcdefgh\nabcdefghi\nabcdefghij\n"},
+		{input{"./testlog/tailtestLF.log", true, 15, 10}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\nabcdefgh\nabcdefghi\nabcdefghij\n"},
+		{input{"./testlog/tailtestLF.log", false, 3, 1024}, "abcdefghi\nabcdefghij\n\n"},
+		{input{"./testlog/tailtestLF.log", false, 15, 1024}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\n\nabcdefgh\n\nabcdefghi\nabcdefghij\n\n"},
+		{input{"./testlog/tailtestLF.log", true, 3, 1024}, "abcdefgh\nabcdefghi\nabcdefghij\n"},
+		{input{"./testlog/tailtestLF.log", true, 4, 1024}, "abcdefg\nabcdefgh\nabcdefghi\nabcdefghij\n"},
 	}
 
-	for _, test := range tests {
+	for no, test := range tests {
 		output := &bytes.Buffer{}
 		mytail(test.in.filename, test.in.ignoreBlankLineFlag, test.in.N, output, test.in.bufsize)
 		if output.String() != test.want {
-			t.Errorf("[%v] test error\n", test.no)
+			t.Errorf("[%v] test error\n", no)
 			t.Logf("output[%v]\n", output.String())
 			t.Logf("want[%v]\n", test.want)
 		}
