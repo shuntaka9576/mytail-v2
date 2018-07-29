9170f72fadb690efe92d0077d31c364448921fa7
shutaka9576 <shuntaka9576@gmail.com>
2018-07-29 10:24:04 +0900

    テスト修正中

--- mytail_test.go
+++ mytail_test.go
@@ -10,31 +10,28 @@ func TestMyTail(t *testing.T) {
 		filename            string
 		ignoreBlankLineFlag bool
 		N                   int
-		bufsize             int64
 	}
 	var tests = []struct {
 		in   input
 		want string
 	}{
-		{input{"./testlog/tailtestLF.log", false, 3, 10}, "abcdefghi\nabcdefghij\n\n"},
-		{input{"./testlog/tailtestLF.log", false, 15, 10}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\n\nabcdefgh\n\nabcdefghi\nabcdefghij\n\n"},
-		{input{"./testlog/tailtestLF.log", true, 3, 10}, "abcdefgh\nabcdefghi\nabcdefghij\n"},
-		{input{"./testlog/tailtestLF.log", true, 4, 10}, "abcdefg\nabcdefgh\nabcdefghi\nabcdefghij\n"},
-		{input{"./testlog/tailtestLF.log", true, 15, 10}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\nabcdefgh\nabcdefghi\nabcdefghij\n"},
-		{input{"./testlog/tailtestLF.log", false, 3, 1024}, "abcdefghi\nabcdefghij\n\n"},
-		{input{"./testlog/tailtestLF.log", false, 15, 1024}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\n\nabcdefgh\n\nabcdefghi\nabcdefghij\n\n"},
-		{input{"./testlog/tailtestLF.log", true, 3, 1024}, "abcdefgh\nabcdefghi\nabcdefghij\n"},
-		{input{"./testlog/tailtestLF.log", true, 4, 1024}, "abcdefg\nabcdefgh\nabcdefghi\nabcdefghij\n"},
+		{input{"./testlog/tailtestLF.log", false, 3}, "abcdefghi\nabcdefghij\n\n"},
+		{input{"./testlog/tailtestLF.log", false, 15}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\n\nabcdefgh\n\nabcdefghi\nabcdefghij\n\n"},
+		{input{"./testlog/tailtestLF.log", true, 3}, "abcdefgh\nabcdefghi\nabcdefghij\n"},
+		{input{"./testlog/tailtestLF.log", true, 4}, "abcdefg\nabcdefgh\nabcdefghi\nabcdefghij\n"},
+		{input{"./testlog/tailtestLF.log", true, 15}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\nabcdefgh\nabcdefghi\nabcdefghij\n"},
 	}
 
+	bufsizePattern := []int64{10, 100, 1024, 2048, 10240}
 	for no, test := range tests {
-		output := &bytes.Buffer{}
-		mytail(test.in.filename, test.in.ignoreBlankLineFlag, test.in.N, output, test.in.bufsize)
-		if output.String() != test.want {
-			t.Errorf("[%v] test error\n", no)
-			t.Logf("output[%v]\n", output.String())
-			t.Logf("want[%v]\n", test.want)
+		for _, bufsize := range bufsizePattern {
+			output := &bytes.Buffer{}
+			mytail(test.in.filename, test.in.ignoreBlankLineFlag, test.in.N, output, bufsize)
+			if output.String() != test.want {
+				t.Errorf("no[%v] bufsize[%v] test error\n", no, bufsize)
+				t.Logf("output[%v]\n", output.String())
+				t.Logf("want[%v]\n", test.want)
+			}
 		}
 	}
-
 }
