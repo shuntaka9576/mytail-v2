3d1390aaa32a61756362ee3f931dc5aa45c7e8ae
shutaka9576 <shuntaka9576@gmail.com>
2018-07-29 10:00:52 +0900

    テスト作成中

--- mytail_test.go
+++ mytail_test.go
@@ -2,90 +2,33 @@ package main
 
 import (
 	"bytes"
-	"os"
 	"testing"
 )
 
-/*
-	test
-*/
-
-func TestMytailStandardoutPut3(t *testing.T) {
-	expect := `		size -= bufsize
+func TestMyTail(t *testing.T) {
+	type input struct {
+		filename            string
+		ignoreBlankLineFlag bool
+		N                   int
+		bufsize             int64
 	}
-}
-`
-	output := new(bytes.Buffer)
-	mytail("testlog.log", false, 3, output, 10)
-	if output.String() != expect {
-		t.Errorf("Error %v\n\n%v", expect, output.String())
+	var tests = []struct {
+		no int
+		in   input
+		want string
+	}{
+		{1, input{"./testlog/tailtestLF.log", false, 3, 10}, "abcdefghi\nabcdefghij\n\n"},
+		{2, input{"./testlog/tailtestLF.log", false, 15, 10}, "a\nab\nabcd\nabcde\nabcdef\nabcdefg\n\nabcdefgh\n\nabcdefghi\nabcdefghij\n\n"},
 	}
-}
 
-func TestMytailStandardOutput10(t *testing.T) {
-	mytail("testlog.log", false, 10, os.Stdout, 10)
-}
-
-func TestMytailBlackLine(t *testing.T) {
-	mytail("./testlog/blankline.log", true, 3, os.Stdout, 10)
-}
-
-// 3行しかないテキストに対して6行要求する
-func TestMytail3Line(t *testing.T) {
-	expect := `aaa
-aaaaa
-aaaaaaaa
-`
-	output := new(bytes.Buffer)
-	mytail("./testlog/3lines.log", true, 6, output, 10)
-	if output.String() != expect {
-		t.Errorf("Error [%v]\n\n[%v]", expect, output.String())
+	for _, test := range tests {
+		output := &bytes.Buffer{}
+		mytail(test.in.filename, test.in.ignoreBlankLineFlag, test.in.N, output, test.in.bufsize)
+		if output.String() != test.want {
+			t.Errorf("[%v] test error\n", test.no)
+			t.Logf("output[%v]\n", output.String())
+			t.Logf("want[%v]\n", test.want)
+		}
 	}
-}
 
-/*
-	benchmark
-*/
-const benchN = 100
-
-func BenchmarkTailBuf1(b *testing.B) {
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 1)
-	}
-}
-
-func BenchmarkTailBufbench10(b *testing.B) {
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 10)
-	}
-}
-
-func BenchmarkTailBuf1024(b *testing.B) {
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 1024)
-	}
-}
-
-func BenchmarkTailBuf2048(b *testing.B) {
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 2048)
-	}
-}
-
-func BenchmarkTailBuf4000(b *testing.B) {
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 4000)
-	}
-}
-
-func BenchmarkTailBuf10240(b *testing.B) {
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		mytail("testlog.log", false, benchN, &bytes.Buffer{}, 10240)
-	}
 }
