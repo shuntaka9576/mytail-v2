1648738261b2497e475001616b0486b1d570a0f6
shutaka9576 <shuntaka9576@gmail.com>
2018-07-29 15:27:20 +0900

    コメント追加

--- mytail.go
+++ mytail.go
@@ -17,8 +17,10 @@ func main() {
 	flag.Parse()
 	fileNames := flag.Args()
 
+	// 引数で指定されたファイルを、1つずつループする
 	for _, fileName := range fileNames {
 		if len(fileNames) > 1 {
+			// 複数のファイルが引数に来た場合に、ファイル名を出力
 			fmt.Printf("==> %v <==\n", fileName)
 		}
 		// ベンチマークの結果、バッファサイズには4000を指定
@@ -27,22 +29,27 @@ func main() {
 }
 
 func mytail(fileName string, ignoreBlankLineFlag bool, N int, output io.Writer, bufsize int64) {
+	// 出力したい行数+1個目のLFより後のテキストを出力するため
 	lfcount := N + 1
+
+	// テキスト読み込み処理
 	fp, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalf("file(%v) open error %v\n", fileName, err)
 	}
 	defer fp.Close()
 
+	// ファイルサイズを取得
 	info, err := fp.Stat()
 	if err != nil {
 		log.Fatalf("get fileinfo error %v\n", err)
 	}
-
 	size := info.Size()
 
+	// mytailの結果を保存する変数
 	lines := ""
 
+	// 行を出力する処理
 	for lfcount >= 0 {
 		var buf []byte
 		if size-bufsize > 0 {
@@ -54,12 +61,13 @@ func mytail(fileName string, ignoreBlankLineFlag bool, N int, output io.Writer, 
 			size = 0
 		}
 
+		// シークした分データを読み込む
 		_, err := fp.Read(buf)
 		if err != nil {
 			log.Fatalf("read buffer error %v\n", err)
 		}
 
-		// 空行無視モード
+		// 空行無視モードの処理
 		if ignoreBlankLineFlag {
 			buf = bytes.Replace(buf, []byte("\r"), []byte(""), -1)
 			for bytes.Count(buf, []byte("\n\n")) != 0 {
@@ -67,12 +75,14 @@ func mytail(fileName string, ignoreBlankLineFlag bool, N int, output io.Writer, 
 			}
 		}
 
+		// 読み込んだバイトの中のLFの数をカウント
 		bufLfcount := bytes.Count(buf, []byte("\n"))
 		if lfcount <= bufLfcount {
 			for i := len(buf) - 1; i >= 0; i-- {
 				if buf[i] == 10 {
 					lfcount--
 					if lfcount == 0 {
+						// 結果を出力
 						fmt.Fprint(output, string(buf[i+1:])+lines)
 						break
 					}
@@ -83,6 +93,7 @@ func mytail(fileName string, ignoreBlankLineFlag bool, N int, output io.Writer, 
 			lfcount -= bufLfcount
 			lines = string(buf) + lines
 			if size == 0 {
+				// テキストファイルの行数より、出力したい行数が大きい場合は、テキストファイルの内容を全て出力する
 				fmt.Fprint(output, lines)
 				break
 			}
